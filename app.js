abe9912fec18e2547693f892e241442dd3ea7639
Sopata_Rafal <sopaciak@gmail.com>
2021-12-11 12:58:14 +0100

    make it better2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {createServer} = require('https');
 const {readFileSync} = require('fs');
 const {Server} = require('socket.io');
-const PORT = process.env.PORT || 2255;
+const PORT = process.env.PORT || 5000;
 
 var tablicaKodow = [];
 
