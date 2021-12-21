f746fb40ee92e1c24a01b5aaa2a5256efcd9f9c3
RSstudent1 <58364115+RSstudent1@users.noreply.github.com>
2021-12-21 09:08:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {createServer} = require('https');
 const {readFileSync} = require('fs');
 const {Server} = require('socket.io');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 var tablicaKodow = [];
 
